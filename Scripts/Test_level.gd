1f4a27cf0e0affa1bd158294b5a0f432095876de
Dmitriy <corban0xd@gmail.com>
2024-07-20 20:36:43 +0700

    Refactoring and fix code in test_level

--- Scripts/Test_level.gd
+++ Scripts/Test_level.gd
@@ -2,22 +2,21 @@ extends Node2D
 
 var selected_item = null
 var dragging = false
-var current_person = null
 
 signal dragsignal
 
 # Словарь для хранения соответствий между персонажами и предметами
 var correct_pairs = {
-	"Person1_Area": "Item1",
-	"Person2_Area": "Item2",
-	"Person3_Area": "Item3"
+	"Person1": "Item1",
+	"Person2": "Item2",
+	"Person3": "Item3"
 }
 
 # Подсказки для персонажей
 var hints = {
-	"Person1_Area": "Я люблю читать.",
-	"Person2_Area": "Мне нравится слушать музыку.",
-	"Person3_Area": "Я всегда ношу ключи с собой."
+	"Person1": "Я люблю читать.",
+	"Person2": "Мне нравится слушать музыку.",
+	"Person3": "Я всегда ношу ключи с собой."
 }
 
 # Функция для отображения подсказок
@@ -28,57 +27,50 @@ func show_hint(person):
 func _on_Item1_input_event(_viewport, event, _shape_idx):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
 		if event.pressed:
-			selected_item = $Item1
+			selected_item = $Items/Item1
 			dragging = true
 		else:
 			dragging = false
-			if current_person:
-				check_match(current_person.name, selected_item.name)
 
 func _on_Item2_input_event(_viewport, event, _shape_idx):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
 		if event.pressed:
-			selected_item = $Item2
+			selected_item = $Items/Item2
 			dragging = true
 		else:
 			dragging = false
-			if current_person:
-				check_match(current_person.name, selected_item.name)
 
 func _on_Item3_input_event(_viewport, event, _shape_idx):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
 		if event.pressed:
-			selected_item = $Item3
+			selected_item = $Items/Item3
 			dragging = true
 		else:
 			dragging = false
-			if current_person:
-				check_match(current_person.name, selected_item.name)
 
 # Обработчики для каждого персонажа
-func _on_Person1_Area_mouse_entered():
-	current_person = $person1/Person1_Area
+#func _on_Person1_Area_mouse_entered():
+	#show_hint("Person1_Area")
+	#current_person = $Persons/Person1
+#
+#func _on_Person1_Area_mouse_exited():
+	#current_person = null
+#
+#func _on_Person2_Area_mouse_entered():
+	#show_hint("Person2_Area")
+	#current_person = $Persons/Person2
+#
+#func _on_Person2_Area_mouse_exited():
+	#current_person = null
+#
+#func _on_Person3_Area_mouse_exited():
+	#current_person = null
+#
+#func _on_person_3_area_mouse_shape_entered(shape_idx):
+	#show_hint("Person3_Area")
+	#current_person = $Persons/Person3
 
-func _on_Person1_Area_mouse_exited():
-	current_person = null
 
-func _on_Person2_Area_mouse_entered():
-	current_person = $person2/Person2_Area
-
-func _on_Person2_Area_mouse_exited():
-	current_person = null
-
-func _on_Person3_Area_mouse_entered():
-	current_person = $person3/Person3_Area
-
-func _on_Person3_Area_mouse_exited():
-	current_person = null
-
-# Привязка сигналов кнопок
-func _ready():
-	show_hint("Person1_Area")
-	show_hint("Person2_Area")
-	show_hint("Person3_Area")
 
 func _process(_delta):
 	if dragging and selected_item:
@@ -91,5 +83,28 @@ func check_match(person, item):
 		$ResultLabel.text = "Правильно!"
 	else:
 		$ResultLabel.text = "Неправильно, попробуйте снова."
-	selected_item = null
-	dragging = false
+
+
+func _on_person_1_area_entered(area):
+	check_match("Person1", area.name)
+
+func _on_person_2_area_entered(area):
+	check_match("Person2", area.name)
+
+func _on_person_3_area_entered(area):
+	check_match("Person3", area.name)
+
+
+func _on_person_1_input_event(_viewport, event, _shape_idx):
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		show_hint("Person1")
+
+
+func _on_person_2_input_event(_viewport, event, _shape_idx):
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		show_hint("Person2")
+
+
+func _on_person_3_input_event(_viewport, event, _shape_idx):
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		show_hint("Person3")
