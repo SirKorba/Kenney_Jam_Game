c10149abe59795b7d9fa246fa344b376f902170a
Dmitriy <corban0xd@gmail.com>
2024-07-20 21:14:20 +0700

    Fix yes.

--- Scripts/Test_level.gd
+++ Scripts/Test_level.gd
@@ -12,6 +12,12 @@ var correct_pairs = {
 	"Person3": "Item3"
 }
 
+var is_entered = {
+	"Person1": false,
+	"Person2": false,
+	"Person3": false
+}
+
 # Подсказки для персонажей
 var hints = {
 	"Person1": "Я люблю читать.",
@@ -48,30 +54,6 @@ func _on_Item3_input_event(_viewport, event, _shape_idx):
 		else:
 			dragging = false
 
-# Обработчики для каждого персонажа
-#func _on_Person1_Area_mouse_entered():
-	#show_hint("Person1_Area")
-	#current_person = $Persons/Person1
-#
-#func _on_Person1_Area_mouse_exited():
-	#current_person = null
-#
-#func _on_Person2_Area_mouse_entered():
-	#show_hint("Person2_Area")
-	#current_person = $Persons/Person2
-#
-#func _on_Person2_Area_mouse_exited():
-	#current_person = null
-#
-#func _on_Person3_Area_mouse_exited():
-	#current_person = null
-#
-#func _on_person_3_area_mouse_shape_entered(shape_idx):
-	#show_hint("Person3_Area")
-	#current_person = $Persons/Person3
-
-
-
 func _process(_delta):
 	if dragging and selected_item:
 		var mousepos = get_viewport().get_mouse_position()
@@ -86,25 +68,58 @@ func check_match(person, item):
 
 
 func _on_person_1_area_entered(area):
-	check_match("Person1", area.name)
+	if area.name == "Item1":
+		is_entered["Person1"] = true
+	else:
+		is_entered["Person1"] = null
 
 func _on_person_2_area_entered(area):
-	check_match("Person2", area.name)
+	if area.name == "Item2":
+		is_entered["Person2"] = true
+	else:
+		is_entered["Person2"] = null
 
 func _on_person_3_area_entered(area):
-	check_match("Person3", area.name)
+	if area.name == "Item3":
+		is_entered["Person3"] = true
+	else:
+		is_entered["Person3"] = null
 
+func _on_person_1_area_exited(area):
+	if area.name == "Item1":
+		is_entered["Person1"] = false
+
+
+func _on_person_2_area_exited(area):
+	if area.name == "Item2":
+		is_entered["Person2"] = false
+
+
+func _on_person_3_area_exited(area):
+	if area.name == "Item3":
+		is_entered["Person3"] = false
 
 func _on_person_1_input_event(_viewport, event, _shape_idx):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
 		show_hint("Person1")
-
+		if is_entered["Person1"]:
+			check_match("Person1", "Item1")
+		elif is_entered["Person1"] == null:
+			check_match("Person1", " ")
 
 func _on_person_2_input_event(_viewport, event, _shape_idx):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
 		show_hint("Person2")
-
+		if is_entered["Person2"]:
+			check_match("Person2", "Item2")
+		elif is_entered["Person2"] == null:
+			check_match("Person1", " ")
 
 func _on_person_3_input_event(_viewport, event, _shape_idx):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
 		show_hint("Person3")
+		if is_entered["Person3"]:
+			check_match("Person3", "Item3")
+		elif is_entered["Person3"] == null:
+			check_match("Person1", " ")
+
