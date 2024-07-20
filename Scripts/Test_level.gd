de9e621112d0e5c9b0293e2833269cc0ef2d1ffb
Мотя <kolevatov.2005@mail.ru>
2024-07-20 18:42:57 +0700

    Oh hell YEAAAAHx2

--- Scripts/Test_level.gd
+++ Scripts/Test_level.gd
@@ -1,86 +1,91 @@
 extends Node2D
 
-# Переменные для хранения состояния
-var selected_person = null
 var selected_item = null
+var dragging = false
+
+signal dragsignal
 
 # Словарь для хранения соответствий между персонажами и предметами
 var correct_pairs = {
-	"person1": "item1",
-	"person2": "item2",
-	"person3": "item3"
+	"Person1_Area": "Item1",
+	"Person2_Area": "Item2",
+	"Person3_Area": "Item3"
 }
 
 # Подсказки для персонажей
 var hints = {
-	"person1": "Я люблю читать.",
-	"person2": "Мне нравится слушать музыку.",
-	"person3": "Я всегда ношу ключи с собой."
+	"Person1_Area": "Я люблю читать.",
+	"Person2_Area": "Мне нравится слушать музыку.",
+	"Person3_Area": "Я всегда ношу ключи с собой."
 }
 
 # Функция для отображения подсказок
 func show_hint(person):
 	$HintLabel.text = hints[person]
 
-# Обработчики нажатия на кнопки персонажей
-func _on_person_1_pressed():
-	selected_person = "person1"
-	show_hint("person1")
-
-func _on_person_2_pressed():
-	selected_person = "person2"
-	show_hint("person2")
-
-func _on_person_3_pressed():
-	selected_person = "person3"
-	show_hint("person3")
-
-# Обработчики нажатия на кнопки предметов
-func _on_item_1_pressed():
-	selected_item = "item1"
-	check_match()
-
-func _on_item_2_pressed():
-	selected_item = "item2"
-	check_match()
-
-func _on_item_3_pressed():
-	selected_item = "item3"
-	check_match()
-
-# Проверка правильности выбора
-func check_match():
-	if selected_person and selected_item:
-		if correct_pairs[selected_person] == selected_item:
-			$ResultLabel.text = "Правильно!"
+# Обработчики для перетаскивания предметов
+func _on_Item1_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		if event.pressed:
+			selected_item = $Item1
+			dragging = true
 		else:
-			$ResultLabel.text = "Неправильно, попробуйте снова."
-		selected_person = null
-		selected_item = null
+			dragging = false
+
+func _on_Item2_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		if event.pressed:
+			selected_item = $Item2
+			dragging = true
+		else:
+			dragging = false
+
+func _on_Item3_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		if event.pressed:
+			selected_item = $Item3
+			dragging = true
+		else:
+			dragging = false
+
+# Обработчики для каждого персонажа
+func _on_Person1_Area_mouse_entered():
+	if dragging and selected_item:
+		check_match("Person1_Area", selected_item.name)
+
+func _on_Person2_Area_mouse_entered():
+	if dragging and selected_item:
+		check_match("Person2_Area", selected_item.name)
+
+func _on_Person3_Area_mouse_entered():
+	if dragging and selected_item:
+		check_match("Person3_Area", selected_item.name)
 
 # Привязка сигналов кнопок
 func _ready():
-	# Отключаем сигналы перед повторным подключением, чтобы избежать дублирования
-	if $Person1.is_connected("pressed", Callable(self, "_on_person_1_pressed")):
-		$Person1.disconnect("pressed", Callable(self, "_on_person_1_pressed"))
-	$Person1.connect("pressed", Callable(self, "_on_person_1_pressed"))
+	$Item1.connect("input_event", Callable(self, "_on_Item1_input_event"))
+	$Item2.connect("input_event", Callable(self, "_on_Item2_input_event"))
+	$Item3.connect("input_event", Callable(self, "_on_Item3_input_event"))
 
-	if $Person1/Person2.is_connected("pressed", Callable(self, "_on_person_2_pressed")):
-		$Person1/Person2.disconnect("pressed", Callable(self, "_on_person_2_pressed"))
-	$Person1/Person2.connect("pressed", Callable(self, "_on_person_2_pressed"))
+	$person1/Person1_Area.connect("mouse_entered", Callable(self, "_on_Person1_Area_mouse_entered"))
+	$person2/Person2_Area.connect("mouse_entered", Callable(self, "_on_Person2_Area_mouse_entered"))
+	$person3/Person3_Area.connect("mouse_entered", Callable(self, "_on_Person3_Area_mouse_entered"))
 
-	if $Person1/Person3.is_connected("pressed", Callable(self, "_on_person_3_pressed")):
-		$Person1/Person3.disconnect("pressed", Callable(self, "_on_person_3_pressed"))
-	$Person1/Person3.connect("pressed", Callable(self, "_on_person_3_pressed"))
+	# Устанавливаем хинты
+	show_hint("Person1_Area")
+	show_hint("Person2_Area")
+	show_hint("Person3_Area")
 
-	if $Item1.is_connected("pressed", Callable(self, "_on_item_1_pressed")):
-		$Item1.disconnect("pressed", Callable(self, "_on_item_1_pressed"))
-	$Item1.connect("pressed", Callable(self, "_on_item_1_pressed"))
+func _process(delta):
+	if dragging and selected_item:
+		var mousepos = get_viewport().get_mouse_position()
+		selected_item.position = Vector2(mousepos.x, mousepos.y)
 
-	if $Item1/Item2.is_connected("pressed", Callable(self, "_on_item_2_pressed")):
-		$Item1/Item2.disconnect("pressed", Callable(self, "_on_item_2_pressed"))
-	$Item1/Item2.connect("pressed", Callable(self, "_on_item_2_pressed"))
-
-	if $Item1/Item3.is_connected("pressed", Callable(self, "_on_item_3_pressed")):
-		$Item1/Item3.disconnect("pressed", Callable(self, "_on_item_3_pressed"))
-	$Item1/Item3.connect("pressed", Callable(self, "_on_item_3_pressed"))
+# Проверка правильности выбора
+func check_match(person, item):
+	if correct_pairs[person] == item:
+		$ResultLabel.text = "Правильно!"
+	else:
+		$ResultLabel.text = "Неправильно, попробуйте снова."
+	selected_item = null
+	dragging = false
