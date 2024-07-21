39f694a3684cdf16bf2991f83a05e4b8857f2625
Мотя <kolevatov.2005@mail.ru>
2024-07-21 20:49:29 +0700

    Its end?

--- Scripts/Exit_to_mainmenu_button.gd
+++ Scripts/Exit_to_mainmenu_button.gd
@@ -3,4 +3,4 @@ extends TextureButton
 func _pressed():
 	$Click_sound.play()
 	await get_tree().create_timer(0.35).timeout
-	get_tree().change_scene_to_file("res://Scenes/MainMenu.tscn")
+	get_tree().change_scene_to_file("res://Scenes/Levels_menu.tscn")
